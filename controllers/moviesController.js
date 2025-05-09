0572eabff8eede5bee4fd67e1abc5685817a3e46
BassaniDavid <davbassan98@gmail.com>
2025-05-09 16:07:15 +0200

    feat: show with reviews

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -4,7 +4,7 @@ const connection = require('../data/db')
 // index
 function index(req, res) {
 
-    const sql = 'SELECT * FROM `movies`;'
+    const sql = 'SELECT * FROM `movies`';
 
     connection.query(sql, (err, results) => {
 
@@ -14,7 +14,7 @@ function index(req, res) {
             })
         }
 
-        res.json(results)
+        res.json(results);
     })
 
     // res.send('index dei film');
@@ -22,7 +22,50 @@ function index(req, res) {
 
 // show
 function show(req, res) {
-    res.send('show dei film');
+
+    // recupero id dall URL
+    const { id } = req.params
+
+    // prima query
+    const sql = 'SELECT * FROM `movies` WHERE id = ?';
+
+    connection.query(sql, [id], (err, results) => {
+
+        if (err) {
+            return res.status(500).json({
+                errorMessage: 'database connection error'
+            })
+        }
+
+        if (results.length === 0) {
+            return res.status(404).json({
+                errorMessage: 'no movie found',
+                id
+            })
+        }
+
+        const movie = results[0];
+
+        // recensioni
+
+        // seconda query
+        const sql = `SELECT * FROM reviews WHERE movie_id = ?`
+
+        // se è andata a buon fine, eseguo la seconda query rimanendo all interno della prima
+        connection.query(sql, [id], (err, results) => {
+            if (err) {
+                console.log(err);
+            }
+
+            movie.reviews = results;
+
+            res.json(movie);
+        })
+
+
+    })
+
+    // res.send('show dei film');
 }
 
 module.exports = {
