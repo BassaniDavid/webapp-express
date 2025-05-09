78fe7464cf2ac244d4899939f09b853074b63a73
BassaniDavid <davbassan98@gmail.com>
2025-05-09 15:37:03 +0200

    feat: index route

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -3,7 +3,21 @@ const connection = require('../data/db')
 
 // index
 function index(req, res) {
-    res.send('index dei film');
+
+    const sql = 'SELECT * FROM `movies`;'
+
+    connection.query(sql, (err, results) => {
+
+        if (err) {
+            return res.status(500).json({
+                errorMessage: 'database connection errror'
+            })
+        }
+
+        res.json(results)
+    })
+
+    // res.send('index dei film');
 }
 
 // show
